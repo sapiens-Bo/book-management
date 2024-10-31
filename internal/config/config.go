e4dc6bbeabef392e22382bad27b779bc3f0821f2
Dorzh <robnotgood@gmail.com>
2024-10-31 21:29:38 +0300

    Add a config file and code for works with him

---
+++ internal/config/config.go
@@ -0,0 +1,34 @@
+// Package config ...
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+// Config is struct for config file
+type Config struct {
+	Path string `yaml:"path" path-default:"~/Books"`
+}
+
+// MustLoad func loaded and return Config type
+func MustLoad() *Config {
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH is not set")
+	}
+
+	//check if file exists
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config file does not exists: %s", configPath)
+	}
+
+	var cfg Config
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("cannot read config: %s", err)
+	}
+
+	return &cfg
+}
